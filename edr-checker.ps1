21e80c03c806c2700ee8838adae5389972b8091e
Ross <rossbingham.5@gmail.com>
2019-10-17 21:59:37 +0100

    Create edr-checker.ps1

---
+++ edr-checker.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+
+Gets a current process listing and checks it for defensive products.
+
+Author: Ross Bingham (@PwnDexter)
+License: BSD 3-Clause
+Required Dependencies: None
+Optional Dependencies: None
+
+.DESCRIPTION
+
+Gets the name of all currently running process then checks them against a list of known defensive products such as AV's, EDR's and logging tools.
+
+.EXAMPLE
+PS C:\> edr-checker
+
+#>
+
+function edr-checker
+{
+	$edr = @(
+				'carbon',
+				'cb',
+				'crowd',
+				'csagent'
+				'csfalcon',
+				'csshell',
+				'cylance',
+				'cyoptics',
+				'cyupdate',
+				'defendpoint'
+				'PGEPOService'
+				'PGSystemTray',
+				'PrivilegeGuard',
+				'sentinel',
+				'splunk',
+				'sysmon',
+			 )
+	
+	$proc = get-process | select ProcessName | Select-String -Pattern $edr -AllMatches # | % { $_.Matches } | % { $_.Value }
+
+	echo $proc
+}
