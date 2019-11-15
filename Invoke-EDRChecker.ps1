e0b59309aa32355372f02b1dfbb58b04c9c772fd
Ross <rossbingham.5@gmail.com>
2019-11-14 18:12:07 -0800

    Added in force reg and remote capability

--- Invoke-EDRChecker.ps1
+++ Invoke-EDRChecker.ps1
@@ -73,9 +73,13 @@ Optional Dependencies: None
 
 Enumerates the host by querying processes, process metadata, dlls loaded into your current process and each dlls metadata, known install paths, installed services, the registry and running drivers then checks the output against a list of known defensive products such as AV's, EDR's and logging tools.
 
+.PARAMETER ForceReg
+
+Forces registry checks when not running as admin.
+
 .EXAMPLE
 PS C:\> Invoke-EDRChecker
-
+PS C:\> Invoke-EDRChecker -ForceReg
 #>
 
 function Invoke-EDRChecker
@@ -152,6 +156,82 @@ function Invoke-EDRChecker
         {ForEach ($p in $drv -Replace "@{") {Write-Output "[-] $p".Trim("}")}}
         else {Write-Output "[+] No suspicious drivers found"}
     }
+
+    Write-Output ""
+    Write-Output "[!] EDR Checks Complete"
+    Write-Output ""
+
+}
+
+<#
+.SYNOPSIS
+
+Enumerates the remote host and checks it for defensive products.
+
+Author: Ross Bingham (@PwnDexter)
+License: BSD 3-Clause
+Required Dependencies: None
+Optional Dependencies: None
+
+.DESCRIPTION
+
+Enumerates the host by checking process listing, known install paths and installed services then checks the output against a list of known defensive products such as AV's, EDR's and logging tools.
+
+.PARAMETER ComputerName
+
+Specifies the computername to perform the remote checks against.
+
+.EXAMPLE
+PS C:\> Invoke-EDRCheckerRemote -ComputerName <ComputerName>
+#>
+
+function Invoke-EDRCheckerRemote
+{
+
+    param([Parameter()] [ValidateNotNullOrEmpty()] [string] $ComputerName=$(throw "ComputerName is mandatory, please provide a value"))
+
+    $edr = $edr_list
+    
+    Write-Output ""
+    Write-Output "[!] Performing EDR Checks against $ComputerName, remote checks are limited to process listing, common install directories and installed services"
+
+    # Added in connection and authentication check to the target host
+
+    Write-Output ""
+    Write-Output "[!] Checking running processes of $ComputerName"
+    if ($proc = Get-Process -ComputerName $ComputerName | Select-Object ProcessName,Name,Path,Company,Product,Description | Select-String -Pattern $edr -AllMatches)
+    {ForEach ($p in $proc -Replace "@{") {Write-Output "[-] $p".Trim("}")}}
+    else {Write-Output "[+] No suspicious processes found"}
+
+    Write-Output ""
+    Write-Output "[!] Checking running services of $ComputerName"
+    if ($serv = Get-Service -ComputerName $ComputerName | Select-Object Name,DisplayName,ServiceName | Select-String -Pattern $edr -AllMatches)
+    {ForEach ($p in $serv -Replace "@{") {Write-Output "[-] $p".Trim("}")}}
+    else {Write-Output "[+] No suspicious services found"}
+
+    Write-Output ""
+    Write-Output "[!] Checking Program Files on $ComputerName"
+    if ($prog = Get-ChildItem -Path "\\$ComputerName\\c$\\Program Files\\*" | Select-Object Name | Select-String -Pattern $edr -AllMatches)
+    {ForEach ($p in $prog -Replace "@{") {Write-Output "[-] $p".Trim("}")}}
+    else {Write-Output "[+] Nothing found in Program Files"}
+    
+    Write-Output ""
+    Write-Output "[!] Checking Program Files x86 on $ComputerName"
+    if ($prog86 = Get-ChildItem -Path "\\$ComputerName\c$\Program Files (x86)\*" | Select-Object Name | Select-String -Pattern $edr -AllMatches)
+    {ForEach ($p in $prog86 -Replace "@{") {Write-Output "[-] $p".Trim("}")}}
+    else {Write-Output "[+] Nothing found in Program Files x86"}
+
+    Write-Output ""
+    Write-Output "[!] Checking Program Data on $ComputerName"
+    if ($progd = Get-ChildItem -Path "\\$ComputerName\c$\ProgramData\*" | Select-Object Name | Select-String -Pattern $edr -AllMatches)
+    {ForEach ($p in $progd -Replace "@{") {Write-Output "[-] $p".Trim("}")}}
+    else {Write-Output "[+] Nothing found in Program Data"}
+    
+    Write-Output ""
+    Write-Output "[!] Checking installed services on $ComputerName"
+    if ($serv = Get-Service -ComputerName $ComputerName | Select-Object Name,DisplayName,ServiceName | Select-String -Pattern $edr -AllMatches)
+    {ForEach ($p in $serv -Replace "@{") {Write-Output "[-] $p".Trim("}")}}
+    else {Write-Output "[+] No suspicious services found"}
 
     Write-Output ""
     Write-Output "[!] EDR Checks Complete"
